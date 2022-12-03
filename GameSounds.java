5e9ef18a406c96b3c58c11b7e59070f73bc6f275
s4elvale <s4elvale@uni-trier.de>
2022-12-03 13:52:13 +0100

    pre-refactoring Code clean up to get a better overview as refactoring is difficult with to many warnings as the important wants for correct refactoring are overlooked. -STR ALT L for code in Java convention - Pacman: remove unused imports and variables, deprecated Modules and typos, replace an anonymous ActionListener with a lambda and make switch enhanced - GameSounds: remove typos, an unused import statement and make empty catch block ignored - Board.java: remove necessary comments, deprecated modules, unused imports, unused variables, typos. Replace gridSize * 1 and 1 * gridSize with gridSize, switch with enhanced switch, ignore empty catch blocks, simplify ifs,  Set definition and manual Array copy

--- GameSounds.java
+++ GameSounds.java
@@ -1,75 +1,75 @@
 /* Drew Schuster */
-import java.io.*;
+
 import java.net.URL;
 import javax.sound.sampled.*;
 
-
 /* This class controls all sound effects*/
-public class GameSounds{
-    
+public class GameSounds {
+
     Clip nomNom;
     Clip newGame;
     Clip death;
-    /* Keeps track of whether or not the eating sound is playing*/
+    /* Keeps track of whether the eating sound is playing*/
     boolean stopped;
-       
 
-/* Initialize audio files */ 
-    public GameSounds(){
-        stopped=true; 
+
+    /* Initialize audio files */
+    public GameSounds() {
+        stopped = true;
         URL url;
         AudioInputStream audioIn;
-        
-        try{
+
+        try {
             // Pacman eating sound
             url = this.getClass().getClassLoader().getResource("sounds/nomnom.wav");
             audioIn = AudioSystem.getAudioInputStream(url);
             nomNom = AudioSystem.getClip();
             nomNom.open(audioIn);
-            
+
             // newGame        
             url = this.getClass().getClassLoader().getResource("sounds/newGame.wav");
             audioIn = AudioSystem.getAudioInputStream(url);
             newGame = AudioSystem.getClip();
             newGame.open(audioIn);
-            
+
             // death        
             url = this.getClass().getClassLoader().getResource("sounds/death.wav");
             audioIn = AudioSystem.getAudioInputStream(url);
             death = AudioSystem.getClip();
             death.open(audioIn);
 
-        }catch(Exception e){}
+        } catch (Exception ignored) {
+        }
     }
-    
+
     /* Play pacman eating sound */
-    public void nomNom(){
+    public void nomNom() {
         /* If it's already playing, don't start it playing again!*/
         if (!stopped)
-          return;
+            return;
 
-        stopped=false;
+        stopped = false;
         nomNom.stop();
         nomNom.setFramePosition(0);
         nomNom.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
     /* Stop pacman eating sound */
-    public void nomNomStop(){
-        stopped=true;
+    public void nomNomStop() {
+        stopped = true;
         nomNom.stop();
         nomNom.setFramePosition(0);
     }
-    
+
     /* Play new game sound */
-    public void newGame(){
+    public void newGame() {
         newGame.stop();
         newGame.setFramePosition(0);
         newGame.start();
     }
-    
+
     /* Play pacman death sound */
-    public void death(){
+    public void death() {
         death.stop();
         death.setFramePosition(0);
         death.start();
